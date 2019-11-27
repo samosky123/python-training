b1058605a3dfcf483f00fa39b669b8920056ad4e
beertwanger <steve.corp@outlook.com>
2019-11-27 09:14:03 +0000

    os exercise and window (ppk) key added.

---
+++ exercises/ex07_os_and_sys.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+path = os.getcwd()
+
+hostname = input('Enter hostname:')
+
+if hostname.startswith('sw'):
+    new_path = os.path.join(path, hostname)
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+else:
+    sys.exit('Hostname has to start with "sw"')
+
+
+
